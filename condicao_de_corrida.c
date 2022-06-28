e74f8959e90dde3348a8a923343508c1e450510a
Eduardo Marques dos Reis <54919290+EduardoMdR@users.noreply.github.com>
2022-06-27 23:13:34 -0300

    :sparkles: Criando condição de corrida

--- condicao_de_corrida.c
+++ condicao_de_corrida.c
@@ -3,12 +3,30 @@
 #include "pthread.h"
 #include "unistd.h"
 
+int i = 0;
+
+void * funcao1(void * arg){
+  i = 5;
+  pthread_exit(0);    
+}
+
+void * funcao2(void * arg){
+  int y = 10;
+  // printf("O programa executa x=y/0, antes da função 1 fazer i = 5");
+  int x = y/i;
+  printf("Valor de x: %d\n",x);
+  pthread_exit(0);    
+}
 
 int main(){
   printf("Condição de Corrida\n");
 
   pthread_t thread1, thread2;
+  pthread_create(&thread2, NULL, funcao2, NULL);
+  pthread_create(&thread1, NULL, funcao1, NULL);
 
+  pthread_join(thread1, NULL);
+  pthread_join(thread2, NULL);
   printf("Fim");
 
   return 0;
