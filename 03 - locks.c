f34ef9c76a8e871315fb7f58baec9e935a78f439
Eduardo Marques dos Reis <54919290+EduardoMdR@users.noreply.github.com>
2022-06-30 16:10:09 -0300

    :sparkles: Aprendendo Locks

---
+++ 03 - locks.c
@@ -0,0 +1,46 @@
+// Nesse programa se temos diversas thread que tentam acessar e aumentar o contador, 
+// porém pode acontecer o caso de que mais de uma thread pega o mesmo valor do contador,
+// e ambas podem salvar ao mesmo tempo sobrescrevendo a outra thread. Temos uma condição de corrida.
+// Para resolver esse problema é utilizado o lock.
+
+#include "stdio.h"
+#include "stdlib.h"
+#include "pthread.h"
+#include "unistd.h"
+
+#define N 4
+#define X 100000
+
+int contador = 0;
+int contador_unsafe = 0;
+
+//pthread_mutex_t lock_contador = PTHREAD_MUTEX_INITIALIZER;
+
+void * incrementar(void *arg){
+  pthread_exit(0);
+}
+
+int main(){
+  printf("Utilizando Locks\n");
+
+  pthread_t thread[N];
+  int i = 0;
+  int * id;
+
+  for(i = 0; i < N; i++){
+    id = (int *) malloc(sizeof(int));
+    *id = i;
+    pthread_create(&thread[i], NULL, incrementar, (void *) (id));
+  }
+
+  for(i = 0; i < N; i++){
+    pthread_join(thread[i], NULL);
+  }
+
+  printf("Valor final do contador (utilizando lock) = %d\n", contador);
+  printf("Valor final do contador_unsafe = %d\n", contador_unsafe);
+
+  printf("Fim");
+
+  return 0;
+}
