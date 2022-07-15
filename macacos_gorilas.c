13d2d6a22f24f6ea683d80338792a57fecc61ebe
Eduardo Marques dos Reis <54919290+EduardoMdR@users.noreply.github.com>
2022-07-14 22:42:14 -0300

    :sparkles: Desenvolvendo problema

--- macacos_gorilas.c
+++ macacos_gorilas.c
@@ -1,207 +1,106 @@
+// Programa não está completo,
+// Sua lógica está pronta 
+// mas ainda n está funcionando
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <pthread.h>
-#define MA 10
-#define MB 10
-#define GA 1
-#define GB 1
+#include <unistd.h>
 
-void * macacoA(void * meuid);
-void * macacoB (void * meuid);
-void * gorilaA (void * meuid);
-void * gorilaB (void * meuid);
+#define MAXCANIBAIS 20
+// Existe apenas um cozinheiro
+int qtdPorcoes = 0;
 
-pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-pthread_cond_t ma_cond = PTHREAD_COND_INITIALIZER;
-pthread_cond_t mb_cond = PTHREAD_COND_INITIALIZER;
-pthread_cond_t ga_cond = PTHREAD_COND_INITIALIZER;
-pthread_cond_t gb_cond = PTHREAD_COND_INITIALIZER;
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER; /* lock pro contador*/
+pthread_cond_t cond_cozinheiro = PTHREAD_COND_INITIALIZER; 
+pthread_cond_t cond_canibal = PTHREAD_COND_INITIALIZER;
 
+void *canibal(void*meuid);
+void *cozinheiro(int qtdPorcoes);
 
-int mA,mB,gA,gB = 0;
-int gaQuer = 0;
-int gbQuer = 0;
-int turno = 0;                  //0 para macacosA e 1 para macacosB
+// Problema dos canibais
+// Canibais não podem se servir ao mesmo tempo, mas podem comer ao mesm tempo
+// não podem se servir se o caldeirão estiver vazio
+// só podem se servir depois que o cozinheiro terminar de fazer as questões
+// se o caldeirão estiver vazio os canibais esperam dormindo
+// Os cozinheiros só podem encher o caldeirão se ele estiver vazio
+
+// Salvando no aprender para continuar em casa
 
 void main(argc, argv)
 int argc;
 char *argv[];{
   int erro;
-  int i, n, m;
+  int i, numCanibais;
   int *id;
 
-  pthread_t tP[MA];
-  for (i = 0; i < MA; i++)  {
-    id = (int *) malloc(sizeof(int));
-    *id = i;
-    erro = pthread_create(&tP[i], NULL, macacoA, (void *) (id));
+  pthread_t tid[MAXCANIBAIS];
 
-    if(erro)    {
-      printf("erro na criacao do thread %d\n", i);
-      exit(1);
-    }
+  if(argc != 3){
+    printf("erro na chamada do programa: jantar <#canibais> <#comida>\n");
+    exit(1);
   }
+  
+  numCanibais = atoi (argv[1]);                                     // Número de canibais
+  qtdPorcoes = atoi (argv[2]);                                      // Quantidade de porções que o cozinheiro consegue preparar por vez
+  printf("numero de canibais: %d -- quantidade de comida: %d\n", numCanibais, qtdPorcoes);
 
-
-  pthread_t tA[MB];
-  for (i = 0; i < MB; i++){
-    id = (int *) malloc(sizeof(int));
-    *id = i;
-    erro = pthread_create(&tA[i], NULL, macacoB, (void *) (id));
-
-    if(erro)    {
-      printf("erro na criacao do thread %d\n", i);
-      exit(1);
-    }
+  if(numCanibais > MAXCANIBAIS){
+    printf("o numero de canibais e' maior que o maximo permitido: %d\n", MAXCANIBAIS);
+    exit(1);
   }
-  pthread_t tF[GA];
-  for (i = 0; i < GA; i++){
+  
+  for (i = 0; i < numCanibais; i++)  {
     id = (int *) malloc(sizeof(int));
     *id = i;
-    erro = pthread_create(&tF[i], NULL, gorilaA, (void *) (id));
-    if(erro)
-    {
-      printf("erro na criacao do thread %d\n", i);
-      exit(1);
-    }
-  }
+    erro = pthread_create(&tid[i], NULL, canibal, (void *) (id));
 
-
-  pthread_t tGB[GB];
-  for (i = 0; i < GB; i++){
-    id = (int *) malloc(sizeof(int));
-    *id = i;
-    erro = pthread_create(&tGB[i], NULL, gorilaB, (void *) (id));
     if(erro){
       printf("erro na criacao do thread %d\n", i);
       exit(1);
     }
   }
-  pthread_join(tP[0],NULL);
-}	
 
-void * macacoA(void * a){
-  int i = *((int *) a);
-  while(1){
-	sleep(rand()%(i+1));
-
-	pthread_mutex_lock(&mutex);
-    if(mB > 0){
-      turno = 0;
-    }
-    while(turno != 0 || gaQuer != 0 || gbQuer != 0 || mB > 0 || gA > 0 || gB > 0) {
-      pthread_cond_wait(&ma_cond,&mutex);
-    }
-
-    mA++;
-  pthread_mutex_unlock(&mutex);
-
-  printf("Macaco %d passado de A para B \n",i);
-  sleep(1);
-
-	pthread_mutex_lock(&mutex);
-    mA--;
-    printf("Macaco %d terminou de passar de A para B; num: %d\n" ,i,mA);
-    if(mA == 0){
-      pthread_cond_broadcast(&mb_cond);
-      pthread_cond_signal(&ga_cond);
-      pthread_cond_signal(&gb_cond);
-    }
-  pthread_mutex_unlock(&mutex);
-  }
-  pthread_exit(0);
+  cozinheiro(qtdPorcoes);
 }
 
-void * macacoB(void * a){
-  int i = *((int *) a);
-  while(1){
-    sleep(rand()%(i+1));
+void * canibal (void* pi){
+  
+  while(1) {
+    pthread_mutex_lock(&lock);
+    while(qtdPorcoes != 0){
+      pthread_cond_wait(&cond_cozinheiro, &lock);
+    }
+    // ...
+    // Pegar uma porção de comida e acordar o cozinheiro se as porções acabaram
 
-    pthread_mutex_lock(&mutex);
-      if(mA > 0){
-        turno = 1;
-      }
-      while(turno != 1 || gaQuer != 0 || gbQuer != 0 || mA > 0 || gA > 0 || gB > 0) {
-        pthread_cond_wait(&mb_cond,&mutex);
-      }
-
-      mB++;
-    pthread_mutex_unlock(&mutex);
-
-    printf("Macaco %d passado de B para A \n",i);
+    printf("%d: vou comer a porcao que peguei\n", *(int *)(pi));
+    sleep(10);
+    qtdPorcoes--;
+    if(qtdPorcoes == 0){
+      printf("Acordando cozinheiro\n");
+      pthread_cond_signal(&cond_cozinheiro);
+    }
+    pthread_mutex_unlock(&lock);
     sleep(1);
-
-    pthread_mutex_lock(&mutex);
-      mB--;
-      printf("Macaco %d terminou de passar de B para A; num: %d\n" ,i,mB);
-      if(mB == 0){
-        pthread_cond_broadcast(&ma_cond);
-        pthread_cond_signal(&ga_cond);
-        pthread_cond_signal(&gb_cond);
-      }
-    pthread_mutex_unlock(&mutex);
   }
-  pthread_exit(0);
 }
 
-
-void * gorilaA(void * a){
-  int i = *((int *) a);
+void *cozinheiro (int m){
   while(1){
-    sleep(rand()%(i+1)+2);
-
-    pthread_mutex_lock(&mutex);
-      gaQuer++;
-      while(mA > 0 || mB > 0 || gA > 0 || gB > 0) {
-        pthread_cond_wait(&ga_cond,&mutex);
-      }
-      gaQuer--;
-      gA++;
-    pthread_mutex_unlock(&mutex);
-
-    printf("GORILA %d passado de A para B \n",i);
-    sleep(5);
-
-    pthread_mutex_lock(&mutex);
-      gA--;
-      printf("GORILA %d terminou de passar de A para B; num: %d\n",i,gA);
-      pthread_cond_broadcast(&ma_cond);
-      pthread_cond_broadcast(&mb_cond);
-      pthread_cond_signal(&ga_cond);
-      pthread_cond_signal(&gb_cond);
-    pthread_mutex_unlock(&mutex);
+    pthread_mutex_lock(&lock);
+    //dormir enquanto tiver comida
+    while(qtdPorcoes != 0){
+      //printf("\nCozinheiro %d no aguardo, existem ainda %d porções de comida\n",id, comida);
+      pthread_cond_wait(&cond_cozinheiro, &lock);
+    }
+    printf("cozinheiro: vou cozinhar\n");
+    qtdPorcoes++;
+    if(qtdPorcoes >= 10){
+      pthread_cond_broadcast(&cond_canibal);
+    }
+    pthread_mutex_unlock(&lock);
+    sleep(1);
+    //acordar os canibais
   }
-  pthread_exit(0);
-}
-
-
-
-void * gorilaB(void * a){
-  int i = *((int *) a);
-  while(1){
-    sleep(rand()%(i+1)+10);
-
-    pthread_mutex_lock(&mutex);
-      gbQuer++;
-      while(mA > 0 || mB > 0 || gA > 0 || gB > 0) {
-        pthread_cond_wait(&gb_cond,&mutex);
-      }
-      gbQuer--;
-      gB++;
-    pthread_mutex_unlock(&mutex);
-
-    printf("GORILA %d passado de B para A \n",i);
-    sleep(5);
-
-    pthread_mutex_lock(&mutex);
-      gB--;
-      printf("GORILA %d terminou de passar de B para A; num: %d\n" ,i,gB);
-      pthread_cond_broadcast(&ma_cond);
-      pthread_cond_broadcast(&mb_cond);
-      pthread_cond_signal(&ga_cond);
-      pthread_cond_signal(&gb_cond);
-    pthread_mutex_unlock(&mutex);
-  }
-  pthread_exit(0);
 }
